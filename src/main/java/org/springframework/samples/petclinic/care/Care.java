9c8fe29c6f3c28fdae7e49648c4a17c29be4981b
José Antonio  Parejo <japarejo@gmail.com>
2022-01-21 12:02:12 +0100

    ª

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,6 +2,17 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -9,8 +20,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Care {
+@Entity
+@Table
+public class Care extends BaseEntity{
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 5, max = 30)
     String name;
+
+    @NotNull
     String description;
+    
+    @NotEmpty
+    @ElementCollection
+    @ManyToMany(cascade = CascadeType.ALL)
     Set<PetType> compatiblePetTypes;
 }
--- src/main/java/org/springframework/samples/petclinic/care/CareFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareFormatter.java
@@ -1,24 +1,33 @@
 package org.springframework.samples.petclinic.care;
 
 import java.text.ParseException;
+import java.util.List;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CareFormatter implements Formatter<Care>{
 
+    @Autowired
+    CareService careService;
+
     @Override
     public String print(Care object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return object.getName();
     }
 
     @Override
     public Care parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+    
+        if(careService.getCare(text) != null){
+            return careService.getCare(text);
+        }else{
+            throw new ParseException("type not found " + text, 0);
+        }
     }
     
 }
